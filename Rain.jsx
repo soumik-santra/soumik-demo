a57b0a03c151a4021359c22e342b9cc6ba8d4428
soumik-santra <soumik.pappu97@gmail.com>
2024-06-27 14:26:27 +0530

    Add files via upload

---
+++ Rain.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+const Rain =() =>{
+    return(
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-12 p-0 m-0">
+            <h2>Food Menu Item</h2>
+            <button className="btn btn-warning  m-4">All Food</button>
+            <button className="btn btn-warning m-4">Fast Food</button>
+            <button className="btn btn-warning m-4">veg</button>
+            <button className="btn btn-warning m-4">veg</button>
+            <button className="btn btn-warning m-4">Non Veg</button>
+            <button className="btn btn-warning m-4">Break Fast</button>
+            <button className="btn btn-warning m-4">Lunch</button>
+            <button className="btn btn-warning m-4">Dinner</button>
+            <button className="btn btn-warning m-4">Tea</button>
+            <button className="btn btn-warning m-4">Coffe</button>
+            </div>
+            
+           
+        </div>
+
+        <div className="row">
+             <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                    <img src="food1.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+             </div>
+             <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                    <img src="food2.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+             </div>
+             <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                    <img src="food3.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+             </div>
+             <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                    <img src="food4.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+             </div>
+        </div>
+        <div className="row">
+            <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                <img src="food5.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+            </div>
+            <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                <img src="food5.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+            </div>
+            <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                <img src="foodimages.jpg" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+            </div>
+            <div className="col-md-3 col-lg-3 col-sm-12 mt-4">
+                <div className="card">
+                <img src="food6.png" alt="..." height={'200px'} width={'280px'}></img>
+                    <p>Burgur</p>
+                    <p>Fast Food</p>
+                    <p>Price:Rs.120</p>
+                    <button className="btn btn-success">Order Now</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    )
+}
+
+export default Rain
